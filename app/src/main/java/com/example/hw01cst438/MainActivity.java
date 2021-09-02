ad08aeb69fc8d4a0b910436428268ffd38bed81e
Roy Morla <royjmorla@gmail.com>
2021-09-01 21:40:12 -0700

    fixed styling issues

--- app/src/main/java/com/example/hw01cst438/MainActivity.java
+++ app/src/main/java/com/example/hw01cst438/MainActivity.java
@@ -26,9 +26,8 @@ public class MainActivity extends AppCompatActivity {
     Button login;
     EditText username, password;
     private TextView textViewResult;
-    static HashMap<Integer, ArrayList<String>> credential_map = preloadCredentials();
+    static HashMap<Integer, ArrayList<String>> credentialMap = preloadCredentials();
 
-    // hardcoded credentials to access app
     private static HashMap<Integer, ArrayList<String>> preloadCredentials() {
         ArrayList<String> admin = new ArrayList<String>();
         admin.add("admin");
@@ -46,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         admin3.add("3");
 
         HashMap<Integer, ArrayList<String>> result = new HashMap<Integer, ArrayList<String>>();
-
         result.put(1, admin);
         result.put(2, admin2);
         result.put(3, admin3);
@@ -54,14 +52,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static ArrayList<String> searchForUser(String user, String pass) {
-        Iterator i = credential_map.entrySet().iterator();
+        Iterator i = credentialMap.entrySet().iterator();
         while (i.hasNext()) {
             Map.Entry mapElement = (Map.Entry) i.next();
             ArrayList<String> credential = (ArrayList<String>) mapElement.getValue();
             String username = credential.get(0);
             String pw = credential.get(1);
             String uId = credential.get(2);
-
             if (username.equals(user) && pw.equals(pass)) {
                 return credential;
             } else if (username.equals(user)) {
@@ -80,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
         login = findViewById(R.id.button);
         username = findViewById(R.id.editTextTextPersonName);
         password = findViewById(R.id.editTextTextPassword);
-
         login.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
@@ -92,66 +88,51 @@ public class MainActivity extends AppCompatActivity {
     public void login() {
         String user = username.getText().toString().trim();
         String pass = password.getText().toString().trim();
-
+        boolean validUsername = false;
+        boolean validPassword = false;
         if (user.isEmpty() && pass.isEmpty()) {
             Toast.makeText(this, "Please enter a username and password", Toast.LENGTH_LONG).show();
             username.setError("Please enter a username");
             password.setError("Please enter a password");
-        } else if(user.isEmpty()){
+        } else if (user.isEmpty()){
             Toast.makeText(this, "Please enter a username", Toast.LENGTH_LONG).show();
             username.setError("Please enter a username");
-        } else if(pass.isEmpty()){
+        } else if (pass.isEmpty()){
             Toast.makeText(this, "Please enter a password", Toast.LENGTH_LONG).show();
             password.setError("Please enter a password");
         }
-
-        boolean valid_username = false;
-        boolean valid_password = false;
-
+        // Find and get user credentials to compare with inputted text
         ArrayList<String> credential = searchForUser(user, pass);
-
         if (credential == null) {
             Toast.makeText(this, "User not found", Toast.LENGTH_LONG).show();
             username.setError("The user was not found");
         }
-
         if (credential.get(0).equals(user)) {
-            valid_username = true;
+            validUsername = true;
         }
-
         if (credential.size() < 3) {
             Toast.makeText(this, "Password is incorrect", Toast.LENGTH_LONG).show();
             password.setError("Password is incorrect");
+        } else if (credential.get(1).equals(pass)) {
+            validPassword = true;
         }
-        else if (credential.get(1).equals(pass)) {
-            valid_password = true;
-        }
-
-        if (valid_username && valid_password) {
+        if (validUsername && validPassword) {
             Toast.makeText(this, "Welcome " + credential.get(0) + "#" + credential.get(2), Toast.LENGTH_LONG).show();
             setContentView(R.layout.post);
-
             textViewResult = findViewById(R.id.textViewPost);
-
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl("https://jsonplaceholder.typicode.com/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
-
             JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-
             Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
-
             call.enqueue(new Callback<List<Post>>() {
                 @Override
                 public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-
                     if (!response.isSuccessful()) {
                         textViewResult.setText("Code: " + response.code());
                     }
-
                     List<Post> posts = response.body();
-
                     for (Post post : posts) {
                         if(post.getUserId() == Integer.parseInt(credential.get(2))) {
                             String content = "";
@@ -159,19 +140,15 @@ public class MainActivity extends AppCompatActivity {
                             content += "User Id:" + post.getUserId() + "\n";
                             content += "Title" + post.getTitle() + "\n";
                             content += "Text: " + post.getText() + "\n\n";
-
                             textViewResult.append(content);
                         }
                     }
                 }
-
                 @Override
                 public void onFailure(Call<List<Post>> call, Throwable t) {
                     textViewResult.setText(t.getMessage());
                 }
             });
         }
-
-
     }
 }
